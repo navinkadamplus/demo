af84493a4d7d3ae6667f619b4fdef26655376b15
navinkadamplus <navin.kadam@analogyplus.com>
2019-03-18 21:55:58 +0530

    changes app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 import DragDrop from './Component/DragDrop/DragDrop';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <DragDrop />
       </div>
     );
